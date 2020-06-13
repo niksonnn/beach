c1ea053a941d6ee5c7dbce6608e1541ee227bf3d
niksonnn <nik.nik.nosov.1985@mail.ru>
2020-06-13 20:03:17 +0400

    change auth and change base

--- beach/settings.py
+++ beach/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
