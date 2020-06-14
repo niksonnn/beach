8d6e017b0fe480e3c0a7a2e08524633ae3a98406
niksonnn <nik.nik.nosov.1985@mail.ru>
2020-06-14 10:55:21 +0400

    change db, add postgresql

--- beach/settings.py
+++ beach/settings.py
@@ -82,11 +82,12 @@ WSGI_APPLICATION = 'beach.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         #'NAME': 'sunrise',
-        #'USER': 'sunrise',
-        #'PASSWORD': '',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'sunrise',
+        'USER': 'sunrise',
+        'PASSWORD': 'sun',
     }
 }
 
